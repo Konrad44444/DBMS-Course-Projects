0b6b102a6318918d3758c8ac1b7e1143914133f0
aizzy1337 <arekzaj7@gmail.com>
2024-03-27 20:22:28 +0100

    Initial frontend setup

--- restaurant-fronted/src/App.tsx
+++ restaurant-fronted/src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Menu from './components/Menu';
+import { ConfigProvider, Flex, Layout, theme } from 'antd';
+import { calc } from 'antd/es/theme/internal';
+
+const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConfigProvider
+    theme={{
+      algorithm: theme.defaultAlgorithm,
+    }}
+    >
+      
+              <Routes>
+                <Route path="/menu" element={ <Menu/> } />
+              </Routes>
+
+    </ConfigProvider>
   );
 }
 
