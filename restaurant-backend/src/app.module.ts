8940cbd4eeede4819ddeb910f50ef957c980fd48
Konrad44444 <konrad_warzecha@wp.pl>
2024-04-03 18:18:53 +0200

    customer module setup

--- restaurant-backend/src/app.module.ts
+++ restaurant-backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './modules/prisma/prisma.module';
 import { DishModule } from './modules/dish/dish.module';
 import { IngredientModule } from './modules/ingredient/ingredient.module';
 import { OrderModule } from './modules/order/order.module';
+import { CustomerModule } from './modules/customer/customer.module';
 
 @Module({
-  imports: [PrismaModule, DishModule, IngredientModule, OrderModule],
+  imports: [PrismaModule, DishModule, IngredientModule, OrderModule, CustomerModule],
   controllers: [AppController],
   providers: [AppService],
 })
