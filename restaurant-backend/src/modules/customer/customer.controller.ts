02acc983ed0d913be06408f80200e377457eb45a
Konrad44444 <konrad_warzecha@wp.pl>
2024-04-03 18:44:21 +0200

    customer basic endpoints

--- restaurant-backend/src/modules/customer/customer.controller.ts
+++ restaurant-backend/src/modules/customer/customer.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CustomerService } from './customer.service';
+import { CreateCustomerDto } from './dto/CreateCustomer.dto';
 
 @Controller('customer')
-export class CustomerController {}
+export class CustomerController {
+  constructor(private readonly customerService: CustomerService) {}
+
+  @Get()
+  async getAllCustomers() {
+    return this.customerService.getAllCustomers();
+  }
+
+  @Get(':id')
+  async getCustomerById(@Param('id', ParseIntPipe) id: number) {
+    return this.customerService.getCustomerById(id);
+  }
+
+  @Post()
+  async saveCustomer(@Body() customer: CreateCustomerDto) {
+    return this.customerService.saveCustomer(customer);
+  }
+
+  @Put(':id')
+  async updateCustomer(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() customer: CreateCustomerDto,
+  ) {
+    return this.customerService.updateDustomer(id, customer);
+  }
+
+  @Delete(':id')
+  async deleteCustomer(@Param('id', ParseIntPipe) id: number) {
+    return this.customerService.deleteCustomer(id);
+  }
+}
--- restaurant-backend/src/modules/customer/customer.service.ts
+++ restaurant-backend/src/modules/customer/customer.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateCustomerDto } from './dto/CreateCustomer.dto';
 
 @Injectable()
-export class CustomerService {}
+export class CustomerService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getAllCustomers() {
+    return this.prisma.customer.findMany();
+  }
+
+  async getCustomerById(id: number) {
+    return this.prisma.customer.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async saveCustomer(customer: CreateCustomerDto) {
+    return this.prisma.customer.create({
+      data: customer,
+    });
+  }
+
+  async updateDustomer(id: number, customer: CreateCustomerDto) {
+    return this.prisma.customer.update({
+      where: {
+        id: id,
+      },
+      data: customer,
+    });
+  }
+
+  async deleteCustomer(id: number) {
+    return this.prisma.customer.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
