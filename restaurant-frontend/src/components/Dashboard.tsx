7c395fd939f941edfc8b3d144ff55b2e1a0bedd1
Konrad44444 <konrad_warzecha@wp.pl>
2024-04-12 15:30:28 +0200

    dashboard finished

--- restaurant-frontend/src/components/Dashboard.tsx
+++ restaurant-frontend/src/components/Dashboard.tsx
@@ -10,7 +10,28 @@ interface Ingredient {
   threshold: number;
 }
 
-const columns: TableProps<Ingredient>["columns"] = [
+interface Order {
+  id: number;
+  totalAmount: number;
+  date: string;
+  customerId: 1;
+  customer: {
+    id: number;
+    name: string;
+    email: string;
+  };
+  dishes: [
+    {
+      dish: {
+        name: string;
+        price: number;
+      };
+      quantity: number;
+    }
+  ];
+}
+
+const ingredientsColumns: TableProps<Ingredient>["columns"] = [
   {
     title: "Name",
     dataIndex: "name",
@@ -35,14 +56,58 @@ const columns: TableProps<Ingredient>["columns"] = [
     title: "Threshold",
     dataIndex: "threshold",
     key: "threshold",
+    render: (_, record) => (
+      <Typography style={{ color: "cornflowerBlue", fontWeight: "bold" }}>
+        {record.threshold}
+      </Typography>
+    ),
   },
   {
-    render: () => <Tag color={"volcano"}>{"ACTION REQUIRED"}</Tag>,
+    render: () => <Tag color={"red"}>{"ACTION REQUIRED"}</Tag>,
+  },
+];
+
+const orderColumns: TableProps<Order>["columns"] = [
+  {
+    title: "Customer Name",
+    key: "customerName",
+    render: (_, record) => <Typography>{record.customer.name}</Typography>,
+  },
+  {
+    title: "Customer Email",
+    key: "customerEmail",
+    render: (_, record) => <Typography>{record.customer.email}</Typography>,
+  },
+  {
+    title: "Date",
+    key: "date",
+    render: (_, record) => <Typography>{record.date.split("T")[0]}</Typography>,
+  },
+  {
+    title: "Total amount",
+    dataIndex: "totalAmount",
+    key: "totalAmount",
+  },
+  {
+    title: "Dishes",
+    key: "dishes",
+    render: (_, record) => (
+      <>
+        {record.dishes.map((dish, index) => {
+          return (
+            <Typography key={index}>
+              {dish.dish.name} - {dish.quantity}
+            </Typography>
+          );
+        })}
+      </>
+    ),
   },
 ];
 
 function Dashboard() {
   const [ingredients, setIngredients] = useState<Ingredient[]>([]);
+  const [orders, setOrders] = useState<Order[]>([]);
 
   useEffect(() => {
     fetch("http://localhost:8080/ingredient", {
@@ -50,11 +115,32 @@ function Dashboard() {
     })
       .then((response) => response.json())
       .then((data) => {
+        let d: Ingredient[] = [];
         data.map((ingredient: Ingredient) => {
           if (ingredient.quantity <= ingredient.threshold) {
-            setIngredients([...ingredients, ingredient]);
+            d.push(ingredient);
           }
         });
+        setIngredients(d);
+      })
+      .catch((error) => console.log(error));
+
+    fetch("http://localhost:8080/order", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        let d: Order[] = [];
+        const date = new Date();
+        const currentDate: string = date.toISOString().split("T")[0];
+        data.map((order: Order) => {
+          const orderDate: string = order.date.split("T")[0];
+
+          if (orderDate === currentDate) {
+            d.push(order);
+          }
+        });
+        setOrders(d);
       })
       .catch((error) => console.log(error));
   }, []);
@@ -62,7 +148,12 @@ function Dashboard() {
   return (
     <div style={{ padding: "40px" }}>
       <Title level={2}>Ingredients below threshold</Title>
-      <Table columns={columns} dataSource={ingredients} />
+      <Table columns={ingredientsColumns} dataSource={ingredients} />
+
+      <hr style={{ opacity: "50%" }} />
+
+      <Title level={2}>Today's orders</Title>
+      <Table columns={orderColumns} dataSource={orders} />
     </div>
   );
 }
